74a0a28c0734bd3b654c9c89fd6a9846fd4d71ad
ratul-space <to.ratul.hosen@gmail.com>
2024-01-08 19:23:07 +0600

    feat: object keyword - Object expressions in kotlin

--- app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -1,25 +1,66 @@
 package com.example.kotlinbasics
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // myObject's super class is 'Any'
-        val myObject = object {
-            var firstName = "Ratul"
-            fun myFullName() = "Ratul Hossain"
+        var btnOne = findViewById<Button>(R.id.button_sent)
+        var onClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(
+                    applicationContext,
+                    "hi from add button, welcome to kotlin world",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
+        btnOne.setOnClickListener(onClickListener)
 
-        Log.d(TAG, "check firstName: ${myObject.firstName}")
-        Log.d(TAG, "check fullName: ${myObject.myFullName()}")
-    }
-
-    companion object {
-        private const val TAG = "MainActivity"
     }
 }
+
+// myObject's super class is 'Any'
+/*val myObject = object {
+    var firstName = "Ratul"
+    fun myFullName() = "Ratul Hossain"
+}
+
+Log.d(TAG, "check firstName: ${myObject.firstName}")
+Log.d(TAG, "check fullName: ${myObject.myFullName()}")*/
+
+//        val widgetOne = Widget("Widget 1")
+//
+//        var widgetClickable = object : Widget("Widget 3"), Clickable {
+//            override fun onClick() {
+//                TODO("Not yet implemented")
+//            }
+//        }
+//
+//
+//    }
+//
+//    companion object {
+//        private const val TAG = "MainActivity"
+//    }
+//}
+//
+//open class Widget(text: String) {
+//    private var myText = text
+//}
+//
+//interface Clickable {
+//    fun onClick()
+//}
+//
+//class ClickableWidget : Widget("Widget 2"), Clickable {
+//    override fun onClick() {
+//        TODO("Not yet implemented")
