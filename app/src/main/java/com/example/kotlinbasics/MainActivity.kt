e867706d201ba3a9afac371c629e2b526852d0ea
ratul-space <to.ratul.hosen@gmail.com>
2024-01-08 14:36:21 +0600

    feat: checking anonymous object access modifier

--- app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +11,15 @@ class MainActivity : AppCompatActivity() {
 
         // myObject's super class is 'Any'
         val myObject = object {
-
+            var firstName = "Ratul"
+            fun myFullName() = "Ratul Hossain"
         }
+
+        Log.d(TAG, "check firstName: ${myObject.firstName}")
+        Log.d(TAG, "check fullName: ${myObject.myFullName()}")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
