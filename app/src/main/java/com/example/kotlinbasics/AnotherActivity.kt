c723c061c88ec3d09c6f97bb525fa64e341197d7
ratul-space <to.ratul.hosen@gmail.com>
2024-01-09 10:25:24 +0600

    feat: use of shape

---
+++ app/src/main/java/com/example/kotlinbasics/AnotherActivity.kt
@@ -0,0 +1,7 @@
+package com.example.kotlinbasics
+
+import android.os.Bundle
+
+class AnotherActivity() {
+
+}
--- app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -1,66 +1,90 @@
 package com.example.kotlinbasics
 
 import android.annotation.SuppressLint
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.core.widgets.Rectangle
+import com.google.android.gms.maps.model.Circle
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = javaClass.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var btnOne = findViewById<Button>(R.id.button_sent)
-        var onClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(
-                    applicationContext,
-                    "hi from add button, welcome to kotlin world",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        }
-        btnOne.setOnClickListener(onClickListener)
+        var shapeOne: Shape = Circle(0, 0)
+        var shapetwo: Shape = Rectangle(100, 100)
+
+        shapeOne.draw()
+        shapetwo.draw()
 
     }
 }
 
-// myObject's super class is 'Any'
-/*val myObject = object {
-    var firstName = "Ratul"
-    fun myFullName() = "Ratul Hossain"
+private fun Shape.draw() {
+
+
 }
 
-Log.d(TAG, "check firstName: ${myObject.firstName}")
-Log.d(TAG, "check fullName: ${myObject.myFullName()}")*/
+var DRAWTAG: String = "SampleDrawing"
 
-//        val widgetOne = Widget("Widget 1")
-//
-//        var widgetClickable = object : Widget("Widget 3"), Clickable {
-//            override fun onClick() {
-//                TODO("Not yet implemented")
-//            }
-//        }
-//
-//
-//    }
-//
-//    companion object {
-//        private const val TAG = "MainActivity"
-//    }
-//}
-//
-//open class Widget(text: String) {
-//    private var myText = text
-//}
-//
-//interface Clickable {
-//    fun onClick()
-//}
-//
-//class ClickableWidget : Widget("Widget 2"), Clickable {
-//    override fun onClick() {
-//        TODO("Not yet implemented")
+abstract class shape(xPos: Int, yPos: Int) {
+
+    var x: Int = xPos
+    var y: Int = yPos
+
+    abstract fun draw()
+}
+
+class Circle(xPos: Int, yPos: Int) : Shape() {
+    override fun draw(canvas: Canvas?, paint: Paint?) {
+        val xPos = 0
+        val x: Int = xPos
+        val yPos = 0
+        var y: Int = yPos
+        Log.d(DRAWTAG, "Drawing circle using circle drawing formula on position (${x}, ${y}).")
+
+    }
+}
+
+class Rectangle(xPos: Int, yPos: Int) : Shape() {
+    override fun draw(canvas: Canvas?, paint: Paint?) {
+        val xPos = 0
+        val x: Int = xPos
+        val yPos = 0
+        var y: Int = yPos
+
+
+        Log.d(
+            DRAWTAG,
+            "Drawing rectangle using rectangle drawing formula on position (${x}, ${y})."
+        )
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
