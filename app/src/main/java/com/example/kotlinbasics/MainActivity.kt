6e44949b3a894102719a7851081fcd429f28db8e
ratul-space <to.ratul.hosen@gmail.com>
2024-01-09 18:44:57 +0600

    feat: uses of open class

--- app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -4,20 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
-
-enum class Answers {
-    YES, NO, NOT_ANSWERED
-}
-
-enum class States {
-    SUCCEEDED,
-    FAILED
-}
-
-enum class Weekdays {
-    SUN, MUN, TUE, WED, THU, FRI, SAT
-}
-
 class MainActivity : AppCompatActivity() {
 
     private val TAG: String = javaClass.simpleName
@@ -25,34 +11,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, giveAnswer(Answers.YES))
-        Log.d(TAG, giveAnswer(Answers.NO))
-        Log.d(TAG, giveAnswer(Answers.NOT_ANSWERED))
-
-        if (cheakState(49) == States.FAILED) Log.d(TAG, "FAILED")
-        if (cheakState(51) == States.SUCCEEDED) Log.d(TAG, "SUCCEEDED")
-
-        for (day in Weekdays.values()) {
-            Log.d(TAG, "${day.name} = ${day.ordinal}")
-        }
-    }
-
-    fun giveAnswer(ans: Answers): String {
-        return when (ans) {
-            Answers.YES -> "Answered: yes"
-            Answers.NO -> "Answered: no"
-            Answers.NOT_ANSWERED -> "Not answered"
-
-        }
-    }
-
-    fun cheakState(value: Int): States {
-        return if (value <= 50) States.FAILED
-        else States.SUCCEEDED
+        var base: Base = Base()
+        base.sayHi()
+        var derived: Derived = Derived()
+        derived.sayHi()
 
     }
 }
 
+
+open class Base {
+    val name: String = "Base"
+    fun sayHi() {
+        Log.d("Base", "hi from base")
+    }
+}
+
+class Derived : Base() {
+
+}
 
 
 
