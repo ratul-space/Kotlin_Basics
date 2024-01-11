fc047f197f1a54368e605f14c33d54d2220ff2a8
ratul-space <to.ratul.hosen@gmail.com>
2024-01-11 08:59:18 +0600

    feat: uses of access modifier

--- app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlinbasics
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +15,21 @@ class MainActivity : AppCompatActivity() {
 
         var personOne = person("Ratul")
         personOne.age = 19
+        personOne.gender = "Male"
         Log.d(TAG, "onCreate person one: name = ${personOne.name}, age = ${personOne.age}")
 
         var personTwo = person("Zoro", 19)
         personTwo.height = 69.0
+        personTwo.gender = "Male"
         Log.d(
             TAG,
             "onCreate person two: name = ${personTwo.name}, age = ${personTwo.age}, height = ${personTwo.height} inch, is teenage = ${personTwo.isTeenager}"
+        )
+        var personThree = person("Anne")
+        personThree.gender = "Female"
+        Log.d(
+            TAG,
+            " onCreate person three: name = ${personThree.name}, Gender = ${personThree.gender}"
         )
 
 
@@ -40,7 +49,6 @@ class person(name: String) {
 
     }
 
-    var gender: String = ""
     private var _height: Double = 0.0
     public var height: Double
         get() = _height
@@ -49,13 +57,33 @@ class person(name: String) {
         }
     var isTeenager: Boolean = false
         private set
+        get() = (age >= 13 && age <= 19)
+
+    var gender: String = ""
         get() {
-            var ret: Boolean = false
-            if (age >= 13 && age <= 19) {
-                ret = true
-            }
-            return ret
+            throw Exception("Gender info is private")
         }
+        set(value) {
+            if (gender.isNullOrBlank()) {
+                throw Exception("Gender must be Male or Female")
+            } else if (value.equals("Male") || value.equals("Female")) {
+                field = value
+            } else {
+                throw Exception("Gender must be Male or Female")
+            }
+
+        }
+
+}
+//    var isTeenager: Boolean = false
+//        private set
+//        get() {
+//            var ret: Boolean = false
+//            if (age >= 13 && age <= 19) {
+//                ret = true
+//            }
+//            return ret
+//        }
 
 
 //    var gender: String = ""
@@ -65,7 +93,7 @@ class person(name: String) {
 //    public fun setGender(gender: String){
 //        this.gender = gender
 //    }
-}
+
 
 
 
