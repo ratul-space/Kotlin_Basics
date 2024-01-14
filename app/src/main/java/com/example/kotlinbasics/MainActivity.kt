2da12131f3a3df121446d14a7de7d47acb441082
ratul-space <to.ratul.hosen@gmail.com>
2024-01-14 10:02:57 +0600

    feat: uses of different funtion

--- app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -13,68 +13,78 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var personOne = person("Ratul")
-        personOne.age = 19
-        personOne.gender = "Male"
-        Log.d(TAG, "onCreate person one: name = ${personOne.name}, age = ${personOne.age}")
+        var original = 20000
+        var added = original.plusTwo()
 
-        var personTwo = person("Zoro", 19)
-        personTwo.height = 69.0
-        personTwo.gender = "Male"
-        Log.d(
-            TAG,
-            "onCreate person two: name = ${personTwo.name}, age = ${personTwo.age}, height = ${personTwo.height} inch, is teenage = ${personTwo.isTeenager}"
-        )
-        var personThree = person("Anne")
-        personThree.gender = "Female"
-        Log.d(
-            TAG,
-            " onCreate person three: name = ${personThree.name}, Gender = ${personThree.gender}"
-        )
-
+        Log.d(TAG, "result = " + added)
 
     }
 }
 
-class person(name: String) {
-    var name: String
-    var age: Int = 0
+fun Int.plusTwo() = this + 200
 
-    init {
-        this.name = name
-    }
+//        var personOne = person("Ratul")
+//        personOne.age = 19
+//        personOne.gender = "Male"
+//        Log.d(TAG, "onCreate person one: name = ${personOne.name}, age = ${personOne.age}")
+//
+//        var personTwo = person("Zoro", 19)
+//        personTwo.height = 69.0
+//        personTwo.gender = "Male"
+//        Log.d(
+//            TAG,
+//            "onCreate person two: name = ${personTwo.name}, age = ${personTwo.age}, height = ${personTwo.height} inch, is teenage = ${personTwo.isTeenager}"
+//        )
+//        var personThree = person("Anne")
+//        personThree.gender = "Female"
+//        Log.d(
+//            TAG,
+//            " onCreate person three: name = ${personThree.name}, Gender = ${personThree.gender}"
+//        )
+//
+//
+//    }
+//}
+//
+//class person(name: String) {
+//    var name: String
+//    var age: Int = 0
+//
+//    init {
+//        this.name = name
+//    }
+//
+//    constructor(name: String, age: Int) : this(name) {
+//        this.age = age
+//
+//    }
+//
+//    private var _height: Double = 0.0
+//    public var height: Double
+//        get() = _height
+//        set(value) {
+//            _height = value
+//        }
+//    var isTeenager: Boolean = false
+//        private set
+//        get() = (age >= 13 && age <= 19)
+//
+//    var gender: String = ""
+//        get() {
+//            throw Exception("Gender info is private")
+//        }
+//        set(value) {
+//            if (gender.isNullOrBlank()) {
+//                throw Exception("Gender must be Male or Female")
+//            } else if (value.equals("Male") || value.equals("Female")) {
+//                field = value
+//            } else {
+//                throw Exception("Gender must be Male or Female")
+//            }
 
-    constructor(name: String, age: Int) : this(name) {
-        this.age = age
-
-    }
-
-    private var _height: Double = 0.0
-    public var height: Double
-        get() = _height
-        set(value) {
-            _height = value
-        }
-    var isTeenager: Boolean = false
-        private set
-        get() = (age >= 13 && age <= 19)
-
-    var gender: String = ""
-        get() {
-            throw Exception("Gender info is private")
-        }
-        set(value) {
-            if (gender.isNullOrBlank()) {
-                throw Exception("Gender must be Male or Female")
-            } else if (value.equals("Male") || value.equals("Female")) {
-                field = value
-            } else {
-                throw Exception("Gender must be Male or Female")
-            }
-
-        }
-
-}
+//        }
+//
+//}
 //    var isTeenager: Boolean = false
 //        private set
 //        get() {
